6da5148b3eeb6c45f14df31e471a730d97a0c938
Adrian Rodriguez <ar9897.r@gmail.com>
2025-03-19 18:36:21 -0500

    database and index action created

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
+import db
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/photos.json')
+def index():
+    return db.photos_all()
